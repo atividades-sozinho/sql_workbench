0b7c0797ae9158133d15d86af7fc89a2c52a8048
Seu Nome <exemplo@seuemail.com.br>
2021-10-04 19:41:36 -0300

    Adicionando o exercicio 4

--- exercicio4.sql
+++ exercicio4.sql
@@ -4,14 +4,14 @@ use exercicio4;
 
 /*Parte do DDL*/
 
-create table paises(
+create table if not exists paises(
 	id_pais int unsigned not null auto_increment,
     nome varchar (100),
-    idioma char(3),
+    idioma char(5),
     PRIMARY KEY (id_pais)
 );
 
-create table estados(
+create table if not exists estados(
 	id_estado int unsigned not null auto_increment,
     id_pais int unsigned not null,
     nome varchar (100),
@@ -19,7 +19,7 @@ create table estados(
     FOREIGN KEY (id_pais) REFERENCES paises (id_pais)
 );
 
-create table cidades(
+create table if not exists cidades(
 	id_cidade int unsigned not null auto_increment,
     id_estado int unsigned not null,
     nome varchar (100),
@@ -27,7 +27,7 @@ create table cidades(
     FOREIGN KEY (id_estado) REFERENCES estados(id_estado)
 );
 
-create table clientes(
+create table if not exists clientes(
 	id_cliente int unsigned not null auto_increment,
     id_cidade int unsigned not null,
     razao_social varchar (100),
@@ -42,7 +42,7 @@ create table clientes(
     FOREIGN KEY (id_cidade) REFERENCES cidades(id_cidade)
 );
 
-create table vendedores(
+create table if not exists vendedores(
 	id_vendedor int unsigned not null auto_increment,
     nome varchar (100),
     telefone char (14),
@@ -51,7 +51,7 @@ create table vendedores(
     PRIMARY KEY (id_vendedor)
 );
 
-create table venda(
+create table if not exists venda(
 	id_venda int unsigned not null auto_increment,
     id_vendedor int unsigned not null,
     id_cliente int unsigned not null,
@@ -62,20 +62,20 @@ create table venda(
     FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
 );
 
-create table unidade_medida(
+create table if not exists unidade_medida(
 	id_unidade_medida int unsigned not null auto_increment,
     sigla char (2),
     descricao varchar (100),
     PRIMARY KEY (id_unidade_medida)
 );
 
-create table grupo_produto(
+create table if not exists grupo_produto(
 	id_grupo_produto int unsigned not null auto_increment,
     descricao varchar (100),
     PRIMARY KEY (id_grupo_produto)
 );
 
-create table produtos(
+create table if not exists produtos(
 	id_produto int unsigned not null auto_increment,
     id_grupo_produto int unsigned not null,
 	descricao varchar (100),
@@ -86,7 +86,7 @@ create table produtos(
     FOREIGN KEY (id_grupo_produto) REFERENCES grupo_produto(id_grupo_produto)
 );
 
-create table item_venda(
+create table if not exists item_venda(
 	id_item_venda int unsigned not null auto_increment,
     id_venda int unsigned not null,
 	id_produto int unsigned not null,
@@ -103,7 +103,7 @@ create table item_venda(
 /*Parte do DML*/
 
 insert into paises (id_pais, nome, idioma)
-values (1, 'Brasil', 'PT');
+values (1, 'Brasil', 'PT-BR');
 insert into paises (id_pais, nome, idioma)
 values (2, 'EUA', 'EN');
 
@@ -118,4 +118,33 @@ values (4, 1, 'São Paulo');
 insert into estados(id_estado, id_pais, nome)
 values (5, 2, 'California');
 
+insert into cidades(id_cidade, id_estado, nome)
+values (1, 1, 'Joaçaba');
+insert into cidades(id_cidade, id_estado, nome)
+values (2, 1, 'Joinville');
+insert into cidades(id_cidade, id_estado, nome)
+values (3, 1, 'Florianópolis');
+insert into cidades(id_cidade, id_estado, nome)
+values (4, 2, 'Curitiba');
+insert into cidades(id_cidade, id_estado, nome)
+values (5, 2, 'Londrina');
+insert into cidades(id_cidade, id_estado, nome)
+values (6, 3, 'Porto Alegre');
+insert into cidades(id_cidade, id_estado, nome)
+values (7, 4, 'São Paulo');
+insert into cidades(id_cidade, id_estado, nome)
+values (8, 5, 'Mountain View');
 
+select id_cliente, cnpj from clientes;
+select id_cliente, situacao from clientes
+	where situacao='A';
+select data_venda from venda
+	where data_venda >= '2012-02-01';
+select id_produto, custo, saldo_estoque from produtos
+	where id_produto=1;
+update venda set situacao = 'F'
+	where id_venda = true and data_venda <= '2012-04-01'; 
+update item_venda set preco_unitario = '3700'
+    where id_produto = 1;
+delete from clientes
+	where id_cliente = true and situacao = 'I';
